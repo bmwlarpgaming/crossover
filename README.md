68e8a57996dd841ec91d1a9d83c1cc1b1d68df15
everythinginitsrightplace <99297055+everythinginitsrightplace@users.noreply.github.com>
2026-05-21 17:12:15 -0500

    Add edited files from totallynotinteresting/crossover

---
+++ README.md
@@ -0,0 +1,23 @@
+# Fork / Rehost of totallynotinteresting/crossover (deleted)
+## The README.md is mostly identical, same as the script, only changes being that it uses this repository.
+## Have fun!
+
+how to run:
+
+umm grant Terminal Full Disk Access. System Settings > Privacy & Security > Full Disk Access(or search "Full Disk Access") and toggle on Terminal
+
+uh open the terminal (cmd + space, type in terminal, and press the one that literally says "Terminal")
+
+then just run curl -fsSL https://raw.githubusercontent.com/everythinginitsrightplace/crossover/main/patch.sh | bash
+
+then open /Applications/CrossOver.app, should all be good, i hope :p
+
+check if all good - in menu bar while in crossover, click "CrossOver" > "Unlock Crossover". It should say - "It will stop working in 9999 days" or smth
+
+if u hav question open an issue, if u have improvements, do not suggest some because i am a cocky nerd (you can still open a pr if u want)
+
+uhh i'll make an uninstaller sometime when i can remember to
+
+to codeweavers: dont charge so much for reskinned wine and this wouldnt happen
+
+[license: MIT](https://github.com/everythinginitsrightplace/crossover/blob/main/LICENSE)
---
+++ patch.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+# forked from totallynotinteresting/crossover
+
+CROSSOVER_MACOS_PATH="/Applications/CrossOver.app/Contents/MacOS"
+REPO_URL="https://github.com/everythinginitsrightplace/crossover.git"
+RAW_URL="https://raw.githubusercontent.com/everythinginitsrightplace/crossover/main"
+RELEASE_URL="https://github.com/everythinginitsrightplace/crossover/releases/latest/download/hook.dylib"
+
+if [ ! -d "$CROSSOVER_MACOS_PATH" ]; then
+    echo "CrossOver.app was not found at $CROSSOVER_MACOS_PATH"
+    echo "please make sure that CrossOver.app is in /Applications/"
+    exit 1
+fi
+
+cd "$CROSSOVER_MACOS_PATH" || exit 1
+
+if git clone "$REPO_URL" crossover_patch; then
+    cd crossover_patch || exit 1
+    # try to build (because who doesnt trust open source right?)
+    echo "building hook.dylib because this contains the logic"
+    if clang -dynamiclib -framework Foundation -framework AppKit -o hook.dylib hook.m; then
+        echo "Build successful."
+    else
+        echo "either somethings gone wrong or you dont have clang installed, so so we're gonna download it from the gh directly"
+        curl -L -o hook.dylib "$RELEASE_URL"
+    fi
+else
+    echo "bruh u dont have git installed, so we gon do some manual downlaoding instead"
+    mkdir -p crossover_patch
+    cd crossover_patch || exit 1
+    
+    echo "this is the thing that kinda makes it work"
+    curl -L -o pco.sh "$RAW_URL/pco.sh"
+    
+    echo "this is the thing that actually does the hooking"
+    curl -L -o hook.dylib "$RELEASE_URL"
+fi
+
+# ok well if it doesnt exist, you've clearly done something wrong
+if [ ! -f hook.dylib ]; then
+    echo "how the hell is hook.dylib not there?"
+    cd ..
+    rm -rf crossover_patch
+    exit 1
+fi
+
+echo "signing it because macos is specal like that"
+codesign -f -s - hook.dylib
+
+echo "blah blah moving it to where it belongs"
+mv hook.dylib ..
+mv ../CrossOver ../CrossOver.o
+echo "gotta resign crossover as well because something about macos doing hardened runtime"
+codesign -f -s - ../CrossOver.o
+mv pco.sh ../CrossOver
+chmod +x ../CrossOver
+
+cd ..
+rm -rf crossover_patch
+
+echo "uh sure try it out"
